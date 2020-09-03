a53116768800f03ec023021ec8c3f635db8d54e2
Karl Palmskog <palmskog@gmail.com>
2020-09-03 07:24:02 -0500

    some binary search range stuff

--- theories/binary_search_range_verif.v
+++ theories/binary_search_range_verif.v
@@ -29,11 +29,11 @@ Definition binary_search_range_while_spec a sh contents from to key :=
 EX low : Z, EX high : Z,
  PROP (from <= low <= to;
        Int.min_signed <= high < to;
-       In key contents -> In key (sublist low (high + 1) contents);
-       ~ In key contents ->
+       In key (sublist from to contents) -> In key (sublist low (high + 1) contents);
+       ~ In key (sublist from to contents) ->
         Forall (fun x => x < key) (sublist from low contents) /\
         Forall (fun x => key < x) (sublist (high + 1) to contents))
- LOCAL (temp _a a; temp _low (Vint (Int.repr low)); temp _high (Vint (Int.repr high)); temp _key (Vint (Int.repr key)))
+ LOCAL (temp _a a; temp _key (Vint (Int.repr key)); temp _low (Vint (Int.repr low)); temp _high (Vint (Int.repr high)))
  SEP (data_at sh (tarray tint (Zlength contents)) (map Vint (map Int.repr contents)) a).
 
 Definition binary_search_spec :=
@@ -63,9 +63,71 @@ Definition main_spec :=
 
 Definition Gprog : funspecs := ltac:(with_library prog [binary_search_range_spec; binary_search_spec; main_spec]).
 
+Lemma In_sorted_gt_sublist : forall x i n l from to low high,
+ 0 <= from <= low ->
+ high <= to <= Zlength l ->
+ sorted (sublist from to l) ->
+ In x (sublist low high l) ->
+ low <= i < high ->
+ Znth i l = n ->
+ n < x ->
+ In x (sublist (i + 1) high l).
+Proof.
+  intros.
+  rewrite sublist_split with (mid := i + 1) in H2; try lia.
+  rewrite in_app in H2; destruct H2; auto.
+  generalize (In_sorted_range low (i + 1) x _ H1); intro X.
+  repeat (lapply X; [clear X; intro X | lia]).
+  replace (i + 1 - 1) with i in X by lia.
+Admitted.
+
 Lemma body_binary_search_range: semax_body Vprog Gprog f_binary_search_range binary_search_range_spec.
 Proof.
 start_function.
+forward.
+forward.
+forward_while (binary_search_range_while_spec a sh contents from to key).
+- unfold binary_search_range_while_spec.
+  entailer!.
+  Exists from; Exists (to - 1).
+  entailer!.
+  split; intros; [|split]; autorewrite with sublist in *.
+  * assumption.
+  * apply Forall_nil.
+  * apply Forall_nil.
+- entailer!.
+- assert (Hle: low <= high) by lia.
+  pose proof (mean_le _ _  Hle) as Hdle.
+  assert (Heq: Int.signed (Int.divs (Int.repr (high - low)) (Int.repr 2)) = (high - low) / 2).
+    unfold Int.divs; rewrite !Int.signed_repr; try rep_lia; [rewrite Zquot.Zquot_Zdiv_pos by lia; reflexivity|].
+    rewrite !Int.signed_repr; try rep_lia.
+    rewrite Zquot.Zquot_Zdiv_pos by lia.
+    rep_lia.
+  assert (Heq': Int.unsigned (Int.add (Int.repr low) (Int.divs (Int.repr (high - low)) (Int.repr 2))) = low + (high - low) / 2).
+    unfold Int.divs; rewrite !Int.signed_repr; try rep_lia.
+    rewrite Zquot.Zquot_Zdiv_pos by lia.
+    rewrite add_repr.
+    rewrite !Int.unsigned_repr; auto.
+    rep_lia.
+  assert (Heq'': Int.signed (Int.add (Int.repr low) (Int.divs (Int.repr (high - low)) (Int.repr 2))) = low + (high - low) / 2).
+    unfold Int.divs; rewrite !Int.signed_repr; try rep_lia.
+    rewrite add_repr.
+    rewrite !Int.signed_repr; [rewrite Zquot.Zquot_Zdiv_pos by lia; reflexivity|].
+    rewrite Zquot.Zquot_Zdiv_pos by lia.
+    rep_lia.
+  forward.
+  * entailer!.
+    destruct H11.
+    unfold Int.mone in H12.
+    apply repr_inj_signed in H12; [congruence|unfold repable_signed|unfold repable_signed]; rep_lia.
+  * forward.
+    forward_if.
+    + forward.
+      entailer!.
+      Exists ((low + ((high - low)/2)) + 1,high).
+      entailer!.
+      split; [lia|]; split; [|split]; intros.
+      -- specialize (H6 H12).
 Admitted.
 
 Lemma body_binary_search: semax_body Vprog Gprog f_binary_search binary_search_spec.
